055019e66b916e2764a1ff81aba1b74ffd19b236
Kam Bielawski <kamtb28@gmail.com>
2020-02-17 09:18:42 -0600

    onMakeEvent function adds event doc in database

--- screens/CalendarScreen.js
+++ screens/CalendarScreen.js
@@ -1,5 +1,7 @@
 import React, { useState } from 'react';
-import { View, StyleSheet, TextInput } from 'react-native';
+import { View, StyleSheet, TextInput, ActivityIndicator } from 'react-native';
+import firebase from 'firebase';
+
 import FloatingButton from '../components/FloatingButton'
 import Calendar from '../components/CalendarStyled';
 
@@ -8,16 +10,48 @@ const CalendarScreen = (props) => {
 
   const [eventName, setEventName] = useState('');
   const [selectedDate, selectDate] = useState('');
+  const [dataLoading, setDataLoading] = useState(false);
+
+  // this function is called when the "Make Event" button is pressed
+  const onMakeEvent = () => {
+    setDataLoading(true);
+    const eventData = {
+      ...selectedDate,
+      title: eventName,
+      people: [],
+    }
+    // this function call makes a new document in the database w/ the above object as its data
+    firebase.firestore().collection('events').doc().set(eventData)
+      .then(() => {
+        setEventName('');
+        selectDate('');
+        setDataLoading(false);
+        props.navigation.navigate('Event', { eventData });
+      })
+      .catch((e) => {
+        console.log(e);
+        setDataLoading(false);
+      });
+  }
 
   return (
     <>
       {
         (eventName && selectDate)
-        && <FloatingButton
-          title="Make Event"
-          onPress={() => console.log({ eventName, selectedDate })}
-          style={styles.floatingButton}
-        />
+          && dataLoading
+          ? <ActivityIndicator
+            style={{
+              position: 'absolute',
+              top: '50%',
+              left: '50%',
+              alignSelf: 'center',
+            }}
+          />
+          : <FloatingButton
+            title="Make Event"
+            onPress={onMakeEvent}
+            style={styles.floatingButton}
+          />
       }
       <View style={styles.container}>
         <TextInput
