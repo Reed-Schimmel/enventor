36bf1b698cf809b4f1793e0eac1bb69cc40d0997
Kam Bielawski <kamtb28@gmail.com>
2020-02-19 09:11:57 -0600

    react navigation documentation

--- navigation/AppNavigator.web.js
+++ navigation/AppNavigator.web.js
@@ -3,6 +3,7 @@ import { createStackNavigator } from 'react-navigation-stack';
 import CalendarScreen from '../screens/CalendarScreen';
 import EventScreen from '../screens/EventScreen';
 
+// React Navigation docs: https://reactnavigation.org/docs/en/getting-started.html
 const MainStackNav = createStackNavigator({
   Home: CalendarScreen,
   Event: EventScreen,
