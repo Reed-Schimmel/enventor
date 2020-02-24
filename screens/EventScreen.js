c0b654d1cc63a4620762921aa431c602863e04fb
Amir Shami <amirshami@ku.edu>
2020-02-23 21:53:00 -0600

    slots change color when selected

--- screens/EventScreen.js
+++ screens/EventScreen.js
@@ -1,16 +1,14 @@
 import React, { Component } from 'react';
-import { View, Text, StyleSheet, ScrollView, SafeAreaView, TouchableOpacity} from 'react-native';
-import { Table, Row, Rows, Col, Cols, TableWrapper } from 'react-native-table-component';
+import { View, Text, StyleSheet, ScrollView, TouchableOpacity } from 'react-native';
+import { Col } from 'react-native-table-component';
 import EventSlot from '../components/EventSlot'
 import FloatingButton from '../components/FloatingButton'
-import { useTheme } from 'react-navigation';
 
 export default class EventScreen extends Component{
   constructor(props){
     super(props);
     this.props = props;
-    this.eventData = props.navigation.getParam('eventData')
-    console.log(this.eventData.title)
+    this.eventData = props.navigation.getParam('eventData') //get the data from the calandar screen
     this.state = {
 
       timeState: true, //if true the table is displayed in 12 hour mode, displayed in 24 hour mode if false
@@ -65,7 +63,7 @@ export default class EventScreen extends Component{
               ['22:00pm'],['22:20pm'],['22:40pm'],
               ['23:00pm'],['23:20pm'],['23:40pm']],
 
-      names: ['Invalid Time Slot', 'Invalid Time Slot', 'Invalid Time Slot', //array of string arrays to track the names of the attendees 
+      names: ['Invalid Time Slot', 'Invalid Time Slot', 'Invalid Time Slot', //array of string arrays to track the names of the attendees st each time slot
               'Invalid Time Slot', 'Invalid Time Slot', 'Invalid Time Slot',
               'Invalid Time Slot', 'Invalid Time Slot', 'Invalid Time Slot',
               'Invalid Time Slot', 'Invalid Time Slot', 'Invalid Time Slot',
@@ -75,28 +73,41 @@ export default class EventScreen extends Component{
               [],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[], [], [], [], [],
               [], [], [], [], [], [], [], [], [], [], [], []],//this.eventdata.availability
 
-      invalidTimes: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 36, 37, 38], //array with the indicies of the invalid time slots 
+      invalidTimes: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 36, 37, 38], //array with the indicies of the invalid time slots. This is used to track which indecies cause an alert to display
 
-      busyTimes: [], //an array that pushes the value of the clicked slots index to track which indices to add a name to 
-      count: 0,
-      //temps: false
+      busyTimes: [], //an array that pushes the value of the clicked slots index to track which indices to add a name to
+
+      trackClicks: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, //tracks the rows clicked to change background to white
+                    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 
+                    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 
+                    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 
+                    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 
+                    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
     }
   }
 
-  // submitTimeSlots()
-  // {
-  //   //called whenever submit is pressed
-  // }
+  /* 
+  submitTimeSlots()
+  {
+    called whenever submit is pressed
+  } 
+  */
+
+
+/**
+ * @pre 12/24 hr button is clicked
+ * @post changes the value of the displayed times array
+*/
 
   changeTime(){
-    let temp = this.state.times
-    let temp2 = this.state.times2
-    let temp3 = this.state.timeState
-    let temp4 = []
+    let temp = this.state.times //call to 12 hr times array (times)
+    let temp2 = this.state.times2 //call to 24 hr times array (times2)
+    let temp3 = this.state.timeState //call to timeState boolean 
+    let temp4 = [] //used to hold values of times so that times can be set to times2 and vice versa
 
     for(let i = 0; i < temp.length; i++)
     {
-      if(temp3=true)
+      if(temp3=true) //check whether to display 12hr array or 24 hr array 
       {
         temp4.push(temp[i])
         temp[i] = temp2[i]
@@ -109,18 +120,24 @@ export default class EventScreen extends Component{
         temp[i] = temp4[i]
       }
     }
-    temp3 = !temp3
+    temp3 = !temp3 //change value of timeState boolean and update the state of the arrays
     this.setState({times: temp})
     this.setState({times2: temp2})
     this.setState({timeState: temp3})
   }
 
-  _alert(index) { 
+
+  /**
+ * @pre a time slot is selected 
+ * @post the state of the busyTimes and trackClicks arrays are updated with the values of the time slot clicked
+ * @param {the index of the clicked on time slot} index
+*/
+  addName(index) { 
     let temp3 = this.state.busyTimes
     let temp4 = this.state.invalidTimes
-    let temp2 = this.state.count
+    let temp = this.state.trackClicks
 
-    for(let i = 0 ; i < temp4.length; i++)
+    for(let i = 0 ; i < temp4.length; i++) //if the index is in the invalidTimes array display an alert
     {
       if(index == temp4[i])
       {
@@ -128,81 +145,99 @@ export default class EventScreen extends Component{
         return
       }
     }
-    if(temp3.length == 0)
+    if(temp3.length == 0) 
     {
-      temp3.push(index)
-      temp2++
+      temp3.push(index) //if busyTimes is empty push the index clicked into the array
+      temp[index] = temp3[0] //add index value to index position in trackClicks array to be able to change background color 
     }
 
-    else
+    else  
     {
       for(let j = 0; j <temp3.length; j++)
       {
         if(temp3[j]==index)
             return   
       }
-      temp3.push(index)
-      temp2++
+      temp3.push(index) //if busyTimes not empty check to see if index already in busyTimes. Push index if no, ignore if yes 
+      temp[index] = temp3[temp3.length-1] //add index value to index position in trackClicks array to be able to change background color 
     }
     this.setState({busyTimes : temp3})
-    this.setState({count: temp2})
+    this.setState({trackClicks : temp})
   }
+
+  /**
+ * @pre at least on time slot is selected, an alert is called otherwise 
+ * @post adds the name given to each of the clicked on time slots 
+*/
     
-  addName(){
+  submitName(){
     let temp = this.state.busyTimes
     let temp2 = this.state.names
+    let temp4 = this.state.trackClicks
     if(temp.length == 0)
+    {
+      alert("Please select a time slot to submit your name")//display alert if submit is clicked with no time slot selected 
       return 
+    }
     
-    let x = prompt("Please enter your name or the name of your event")
+    let x = prompt("Please enter your name") //take in users name
     if(x != null && x.length>0)
     {
       for(let i = 0; i < temp.length; i++)
       {
-        temp2[temp[i]].push(x)
+        temp2[temp[i]].push(x) //loop through busy times to find which index values to add names to 
       } 
-      temp = []
+      
+      for(let i = 0; i < temp4.length; i++)
+      {
+        temp4[i] = 0 //rest the values of trackClicks array so that background color doesn't stay white after name added
+      }
+      temp = [] //empty busyTimes array so that those time slots can be clicked on again 
       this.setState({busyTimes : temp})
       this.setState({names : temp2})
+      this.setState({trackClicks : temp4})
       //this.submitTimeSlots(tableNames2)
     }
   }
     
-    render () {
-
+    render () 
+    {
       const state = this.state
-     // let tom = state.temps
-      const items = state.times.map((times, index)=> (     
+      const items = state.times.map((times, index)=> ( //use map() to display a slot for each value in times array into one variable (items)
       <TouchableOpacity
-        style ={[styles.text, state.names[index] == '' ? styles.text3 : styles.text2,
-        state.names[index]=="Invalid Time Slot" ? {backgroundColor: "grey"}: null,
-        state.busyTimes[state.count-1] == index ? {backgroundColor: "white"} : null]} 
-        key = {index} onClick = {() => this._alert(index)} >
-       <EventSlot time = {state.times[index]} names = {state.names[index]}></EventSlot>
+        style = {[
+        styles.text, state.names[index] != '' ? styles.text2 : styles.text3, //if names at index isn't empty make background green, pink otherwise
+        state.names[index]=="Invalid Time Slot" ? {backgroundColor: "grey"}: null, //if names is 'invalid time slot' at index make background grey
+        state.trackClicks[index] == index && index != 0 ? styles.text4 : null //if a slot is clicked, make background white
+        ]} 
+        key = {index} onClick = {() => this.addName(index)} > 
+        <EventSlot time = {state.times[index]} names = {state.names[index]}></EventSlot> 
       </TouchableOpacity>
-       )
+      )
     );
+
+  return( 
     
-
-
-  return(
-    <>
+     <> {/* use angle brackets around JSX so that the submit button will stay on screen while scrolling */}
     <FloatingButton
           title="Submit"
-          onPress={()=>this.addName()}
+          onPress={()=>this.submitName()}
           style={styles.submitButton}
-        />
+        /> 
        <ScrollView style = {styles.ScrollView}>
-       <FloatingButton
+       <FloatingButton 
           title="12/24"
           onPress={()=>this.changeTime()}
           style={styles.timeButton}
         />
         <View style = {styles.container}>
-          <Text style = {styles.eventTitle}>Amir's Event</Text> 
-          {/* <Text> Availability</Text> */}
+          <Text 
+            style = {styles.eventTitle}>{this.eventData.title} {/* display the event title at the top of the page */}
+          </Text> 
         </View>
-        <Col data = {items}/>
+        <Col 
+          data = {items} //display the time slots in a column format 
+        /> 
       </ScrollView>
         </>
     )
@@ -210,42 +245,40 @@ export default class EventScreen extends Component{
 }
 
 const styles = StyleSheet.create({
-  container: {
+  container: { //style for event name header
     borderWidth: 1,
     height: 50,
     backgroundColor: "lightblue",
     justifyContent: "space-evenly",
-    //flexDirection: "row"
   },
 
   eventTitle: {
     textAlign: "center",
     fontSize: 25,
-    //justifyContent: "space-evenly",
     },
 
-  text: {
+  text: { //style for invalid time slots 
    backgroundColor: 'grey',
    borderWidth: 1,
    height: 50
   },
 
-  text2: {
+  text2: { //styles for time slots with added names 
     backgroundColor: 'lightgreen',
     borderWidth: 1,
     height: 50
   },
-  text3: {
+  text3: { //style for empty time slots
     backgroundColor: 'pink',
 
   },
-  text4: {
+  text4: { //style for selected time slots 
     backgroundColor: "white"
   },
 
   submitButton: {},
 
-  timeButton: {
+  timeButton: { //style for 12-24 hour button 
     position: "relative",
     right: 0,
     bottom: 0,
